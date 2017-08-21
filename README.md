4e7fc11f2b557409d27bb155891739233f3dba3b
Musthaq Ahamad <musthu.gm@gmail.com>
2017-08-21 20:49:37 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Driodo - Driving Assistant
---
+++ app/src/main/java/me/haxzie/driodo/Activities/SplashActivity.java
@@ -0,0 +1,36 @@
+package me.haxzie.driodo.Activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import me.haxzie.driodo.R;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class SplashActivity extends AppCompatActivity {
+    Button btn_start;
+    Context context;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        context = SplashActivity.this;
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(context, MobileVerifActivity.class));
+            }
+        });
+    }
+}
